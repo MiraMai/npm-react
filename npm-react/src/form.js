4172379a2380bc2dda4bf6173738a0e3c6750383
Mira <mira.aeridou@gmail.com>
2017-05-29 15:52:17 +0200

    created onchange functions in form.js

--- npm-react/src/form.js
+++ npm-react/src/form.js
@@ -2,15 +2,36 @@ import React, { Component } from 'react';
 
 
 class Form extends Component {
+  constructor(props) {
+      super(props);
+    
+      this.saveName= this.saveName.bind(this);
+      this.saveEmail= this.saveEmail.bind(this);
+  }
   render() {
     return (
         <div>
-            <input placeholder="name" />
-            <input placeholder="email" />
+            <input onChange={this.saveName} placeholder="name" />
+            <input onChange={this.saveEmail} placeholder="email" />
         </div>
       
     );
   }
+    saveName (event) {
+        let name= event.target.value;
+        this.setState ({
+            name: name
+        });
+         console.log(name);
+
+    }
+     saveEmail (event) {
+        let email= event.target.value;
+        this.setState ({
+            email: email
+        });
+         console.log(email);
+    }
 }
 
 export default Form;
